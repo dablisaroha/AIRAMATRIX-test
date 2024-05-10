c269b74a45628fe8ecd03cd55889b2bcc76a9928
Dablisaroha <saroha.dabli@gmail.com>
2024-05-10 18:59:40 +0530

    Two scenarios for dynamic dropdowns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,9 +16,9 @@ export class AppComponent implements OnInit {
   output: any = {}
   tree: any = {}
   form: FormGroup;
-  States: any[] = []
-  districts: any[] = []
-  places: any[] = []
+  preformatForm: FormGroup;
+  countries: any[] = [];
+
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       country: [''],
@@ -26,12 +26,69 @@ export class AppComponent implements OnInit {
       district: [''],
       place: ['']
     });
+    this.preformatForm = this.fb.group({
+      country: [''],
+      state: [''],
+      district: [''],
+      place: ['']
+    });
+    this.form.get('country')?.valueChanges.subscribe(() => {
+      this.form.get('state')?.setValue(null); // Reset state field
+      this.form.get('district')?.setValue(null); // Reset district field
+      this.form.get('place')?.setValue(null); // Reset place field
+    });
+    this.form.get('state')?.valueChanges.subscribe(() => {
+      this.form.get('district')?.setValue(null); // Reset district field
+      this.form.get('place')?.setValue(null); // Reset place field
+    });
+    this.form.get('district')?.valueChanges.subscribe(() => {
+      this.form.get('place')?.setValue(null); // Reset place field
+    });
+
+
+
+
+    // preformmatted form reset field logic
+    this.preformatForm.get('country')?.valueChanges.subscribe(() => {
+      this.preformatForm.get('state')?.reset();
+      this.preformatForm.get('district')?.reset();
+      this.preformatForm.get('place')?.reset();
+    });
+
+    this.preformatForm.get('state')?.valueChanges.subscribe(() => {
+      this.preformatForm.get('district')?.reset();
+      this.preformatForm.get('place')?.reset();
+    });
+
+    this.preformatForm.get('district')?.valueChanges.subscribe(() => {
+      this.preformatForm.get('place')?.reset();
+    });
   }
   ngOnInit(): void {
     this.tree = this.buildTreePattern(this.firstLevelArr, this.secondLevelArr, this.thirdLevelArr, this.fourthLevelArr);
     this.output = { countries: this.tree };
-    console.log(1, JSON.stringify(this.tree)); // console for completed tree structure for given input
+    console.log('tree', JSON.stringify(this.output)); // console for completed tree structure for given input
+    this.countries = Object.values(this.tree);
+    this.getAnotherFormat()
   }
+
+
+  getAnotherFormat(){
+    this.countries = Object.values(this.output.countries).map((country: any) => {
+      const statesArray = Object.values(country.states).map((state: any) => {
+          const districtsArray = Object.values(state.districts).map((district:any) => {
+              const placesArray = Object.values(district.places).map((place: any) => {
+                  return { placeName: place.placeName };
+              });
+              return { districtName: district.districtName, places: placesArray };
+          });
+          return { stateName: state.stateName, districts: districtsArray };
+      });
+      return { countryName: country.countryName, states: statesArray };
+    });
+    
+  }
+
   firstLevelArr = [
     { id: "1", name: "India" },
     { id: "2", name: "Germany" }
@@ -43,6 +100,12 @@ export class AppComponent implements OnInit {
     { id: "s3", parentId: "1", name: "Maharashtra" },
     { id: "s4", parentId: "1", name: "Tamilnadu" }
   ];
+
+  getchildOptions(GParent: any, Parent: any): any {
+    console.log(111111,Object.values(this.form.get(GParent)?.value[Parent]))
+    return Object.values(this.form.get(GParent)?.value[Parent])
+  }
+
 
   thirdLevelArr = [
     { id: "d1", parentId: "s1", name: "Upper Bavaria" },
@@ -100,30 +163,6 @@ export class AppComponent implements OnInit {
       })
     })
     return this.output;
-  }
-
-  onChangeCountry(value: any) {
-    console.log(value)
-    this.States = this.secondLevelArr.filter((ele)=>{
-      return ele.parentId == value.id
-    })
-    this.districts =[]
-    this.places = []
-  }
-
-  onChangeState(value: any) {
-    console.log(value)
-    this.districts = this.thirdLevelArr.filter((ele)=>{
-      return ele.parentId == value.id
-    })
-    this.places = []
-  }
-
-  onChangeDistrict(value: any) {
-    console.log(value)
-    this.places = this.fourthLevelArr.filter((ele)=>{
-      return ele.parentId == value.id
-    })
   }
 
 }
